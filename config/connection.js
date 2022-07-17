59786d86db7a5776cb2e03c619fb353cc6693c2c
Mo <mohamed.abdullahi@yahoo.com>
2022-07-16 23:13:31 -0700

    add connnection to mongoose

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+//Dependancies
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001; 
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require('../routes'));
+
+//Connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log('Connected To localhost:${PORT}'));
