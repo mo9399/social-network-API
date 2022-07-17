c812bbacc3f9014582195c6ab52463e43ac98d24
Mo <mohamed.abdullahi@yahoo.com>
2022-07-17 01:51:56 -0700

    move code from connection to server.js then write connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,5 @@
-//Dependancies
-const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const app = express();
-const PORT = process.env.PORT || 3001; 
-
-//Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(require('../routes'));
-
-//Connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -18,4 +7,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-
 
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log('Connected To localhost:${PORT}'));
+module.exports = mongoose.connection;
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const express = require('express')
-const db = require('./config/connection');
-const routes = require('./routes');
+//Dependancies
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; 
 
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require('./routes'));
+
+//Connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log('Connected To localhost:${PORT}'));
